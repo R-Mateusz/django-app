a472397d28e99fadf3be4f8bddd12c291533fcd3
mateusz <your@example.com>
2024-03-14 19:55:04 +0100

    create Parking, Car models; setup admin.py

--- App/admin.py
+++ App/admin.py
@@ -1,4 +1,25 @@
 from django.contrib import admin
+from .models import Car, Parking
 
-# Register your models here.
+
+"""
+    Setup models with filters
+"""
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['registration_board'
+        , 'registration_start_time'
+        , 'registration_end_time']
+
+    list_filter = ['registration_start_time', 'registration_end_time']
+
+
+@admin.register(Parking)
+class ParkingAdmin(admin.ModelAdmin):
+    list_display = ['parking_sector', 'parking_spot']
+
+    list_filter = ['parking_spot', 'parking_sector']
+
+    ordering = ['parking_sector', 'parking_spot']
+
 
--- App/models.py
+++ App/models.py
@@ -1,2 +1,45 @@
-import django.forms
 from django.db import models
+from django.utils import timezone
+
+"""
+    Variables
+"""
+
+default_start_time = timezone.now
+default_end_time = timezone.now() + timezone.timedelta(days=1)
+CAR_MODEL_CHOICES = {"model_bmw": "BMW",
+                     "model_audi": "Audi",
+                     "model_mercedes": "Mercedes",
+                     "model_volvo": "Volvo",
+                     "model_toyota": "Toyota"}
+
+PARKING_SPOTS = {f"spot_{x}": f"{x}" for x in range(1, 51)}
+
+PARKING_SECTOR = {"sector_a": "A",
+                  "sector_b": "B",
+                  "sector_c": "C",
+                  "sector_d": "D"}
+
+"""
+    Models initialization for parking reservation app
+"""
+
+
+class Parking(models.Model):
+    parking_spot = models.CharField(choices=PARKING_SPOTS, max_length=10)
+    parking_sector = models.CharField(choices=PARKING_SECTOR, max_length=10)
+
+    class Meta:
+        verbose_name_plural = "Parking"
+
+
+class Car(models.Model):
+    car_model = models.CharField(choices=CAR_MODEL_CHOICES, default='model_bmw', blank=False, null=False, max_length=40)
+    registration_board = models.CharField(max_length=9, blank=False, null=False)
+    registration_start_time = models.DateField(default=default_start_time)
+    registration_end_time = models.DateField(default=default_end_time)
+    parking = models.ForeignKey(Parking, on_delete=models.PROTECT, null=False, blank=False, default=None)
+
+    class Meta:
+        verbose_name_plural = "Car"
+
